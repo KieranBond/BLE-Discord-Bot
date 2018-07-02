10930569e20972835fec280055734f6e0bb12310
Tilley661 <Tilley661@mail.com>
2018-07-02 22:24:09 +0100

    added lottery

--- bot.js
+++ bot.js
@@ -29,10 +29,33 @@ client.on('message', msg =>
 		{
 			msg.reply('https://discord.js.org/');
 		}
-		if (command === 'add')
+		if (command === 'lotoadd')
 		{
 			msg.reply('found add');
 			msg.reply('param 3 = ' + params[2]);
+		
+			if (params[2] != undefined) {  //checks para exists
+				lotoArr.push(params[2])    //push new param to list
+			} 
+			console.log('lotoArr = ' + lotoArr); //debug list
+		}
+		if (command === 'lotoclear')
+		{
+			msg.reply('list is now cleared');
+			console.log('list has been cleared');
+		}
+		if (command === 'lotoall')
+		{
+			var i;
+			for (i = 0; i < lotoArr.length; i++){
+				msg.reply(lotoArr[i]);
+			}
+		}
+		if (command === 'lotorand')
+		{
+			var rand = Math.floor(Math.random() * lotoArr.length); 
+			msg.reply("And the winner is!");
+			msg.reply(lotoArr[rand]);
 		}
 	}
 });
