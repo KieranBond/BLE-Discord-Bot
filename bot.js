9c107af95b45ff8f2a1f4c35ea7319d009605b0d
KieranB <Kieranbond97@gmail.com>
2018-07-02 21:41:40 +0100

    Updated gitignore.

--- bot.js
+++ bot.js
@@ -6,10 +6,17 @@ client.on('ready', () =>
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pongMOTHER FUCKER');
-  }
+client.on('message', msg => 
+{
+	if(msg.content.startsWith(prefix))
+	{
+		var command = msg.content.slice(prefix.length, msg.content.length);
+		console.log("Command : " + command);
+	}
+	if (msg.content === 'ping') 
+	{
+		msg.reply('pongMOTHER FUCKER');
+	}
 });
 
 client.login('NDYzNDM2OTcyNTMxMzg0MzIx.DhwaRg.Prg9mgzyZtv8qPBCYxwvpogQscE');
