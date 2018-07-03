bba212f9af5bbb262284f68b5e3618a99181f4f6
Tilley661 <Tilley661@mail.com>
2018-07-03 08:44:01 +0100

    changed to switch and added default

--- bot.js
+++ bot.js
@@ -17,11 +17,107 @@ client.on('message', msg =>
 	if(msg.content.startsWith(prefix))
 	{
 		const channel = msg.channel;
-		
 		var params = msg.content.split(' '); //gets all values into an array. Including the command at 1 and prefix at 0..
 		var command = params[1].toLowerCase(); //second in array
 		
 		console.log("Command : " + command);
+		
+		//change to switch - read https://www.oreilly.com/library/view/high-performance-javascript/9781449382308/ch04s02.html
+		//not tested the below as i cant run the node from work.... actually ill try now
+		switch (command)
+		{
+			case 'help':
+				var helpString = GetHelpString();
+				console.log(helpString);
+				channel.send(helpString);
+				break;
+				
+			case 'ping':
+				msg.reply('pong MOTHER FUCKER');
+				break;
+				
+			case 'api':
+				msg.reply('https://discord.js.org/');
+				break;
+				
+			case 'lottoadd':
+				var addTos = [];
+				if(params[2] != undefined)//Just check there's atleast one
+				{
+					//Adds multiple.
+					for(var i = 2; i < params.length; i++)//Go through params, make this a bit more dynamic.
+					{
+						msg.reply('adding: ' + params[i]);
+						lottoArr.push(params[i]);
+					}
+				}
+				console.log('lottoArr = ' + lottoArr); //debug list
+				break;
+			
+			case 'lottoclear':
+				lottoArr = [];//Essentially wipes the array by creating a new one.
+				msg.reply('list is now cleared');
+				console.log('list has been cleared');	
+				break;
+				
+			case 'lottoall':
+				var allString = "";
+				var i;
+				for (i = 0; i < lottoArr.length; i++)
+				{
+					allString += lottoArr[i] + ", ";
+				}
+				allString = allString.substring(0, allString.length - 2);
+
+				if(allString.length > 0) 
+				{
+					channel.send(allString);
+				}
+				else
+				{
+					channel.send('Lotto list is empty. Use LottoAdd command to add to list.');
+				}
+				break;
+			
+			case 'lottorand':
+				if(lottoArr.length > 0)
+				{
+					var rand = getRandomInt(0, lottoArr.length); 
+					msg.reply("and the winner is: ");
+					channel.send(lottoArr[rand]);
+					console.log(rand + lottoArr[rand]);
+					lastWinner = lottoArr[rand];
+				}
+				else
+				{
+					msg.reply('no lucky draw today. Lotto list is empty!');
+				}
+				break;
+				
+			case 'lottowinner':
+				if(lastWinner != undefined)
+				{
+					msg.reply("last winner was: `" + lastWinner + "`")
+				}
+				else
+				{
+					msg.reply('no winners yet!');
+					if(lottoArr.length > 0)
+					{
+						msg.reply("but the list isn't empty.. Why not give it a spin with LottoRand?");
+					}
+				}
+				break;
+				
+			case default:
+				console.log('no command found');
+				msg.reply("There is no command: " + command + "check your shit");
+		}
+				
+			
+			
+				
+		/*		
 		if(command == 'help')
 		{
 			var helpString = GetHelpString();
@@ -109,6 +205,9 @@ client.on('message', msg =>
 				}
 			}
 		}
+		
+		*/ //might not have this close in right place. cant test at work :(
+		
 	}
 });
 
