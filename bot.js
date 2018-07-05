c7af4af5d45b75073036385a0533a2dd891ce47e
KieranB <Kieranbond97@gmail.com>
2018-07-05 03:36:25 +0100

    Started creating a role permissions system.

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const TilleyTest = require("./scripts/tilleyTest");
 const lottoCommands = require("./commands/lotto");
 
 const prefix = "!ble ";
-const lottoArr = [];
-var lastWinner;
 
+
+var allowedRoles = [];
 
 client.on('ready', () => 
 {
@@ -25,104 +25,151 @@ client.on('message', msg =>
 		var command = params[1].toLowerCase(); //second in array
 		
 		console.log("Command : " + command);
+		var userHasPermissions = false;
 		
-		//change to switch - read https://www.oreilly.com/library/view/high-performance-javascript/9781449382308/ch04s02.html
-		//not tested the below as i cant run the node from work.... actually ill try now
-		switch (command)
+		//Checking if this user has one of the allowed role to use bots.
+		if(allowedRoles.length > 0)
 		{
-			case 'help':
-				var helpString = GetHelpString();
-				console.log(helpString);
-				channel.send(helpString);
-				break;
-				
-			case 'ping':
-				msg.reply('pong MOTHER FUCKER');
-				break;
-				
-			case 'api':
-				msg.reply('https://discord.js.org/');
-				break;
-				
-			case 'lottoadd':
-			
-				var addTos = [];
-				if(params[2] != undefined)//Just check there's atleast one
-				{
-					//Adds multiple.
-					for(var i = 2; i < params.length; i++)//Go through params, make this a bit more dynamic.
-					{
-						addTos[i-2] = params[i];
-					}
-				}
-				
-				lottoCommands.add(addTos);
-				
-				break;
-			
-			case 'lottoclear':
-
-				lottoCommands.clear();
-				msg.react('👍');
-				
-				break;
-				
-			case 'lottoall':
-
-				var lotList = lottoCommands.all();
-				if(lotList != undefined)
-				{
-					channel.send("So far in the Lotto list: " + lottoCommands.all());	
-				}
-				else
-				{
-					channel.send('Lotto list is empty. Use LottoAdd command to add to list.');
-				}
+			console.log('Only specific roles can use commands. ');
 						
-				break;
+			for(var i = 0; i < allowedRoles.length; i++)
+			{
+				var current = allowedRoles[i];
+				console.log('Checking for role: ' + current.name);
+				if(msg.member.roles.has(current.id))
+				{
+					userHasPermissions = true;
+				}
+			}
 			
-			case 'lottorand':
+			if(userHasPermissions === true)
+			{
+				console.log('This user is allowed.');				
+			}
+			else
+			{
+				console.log('This user doesnt have credentials required to use commands.');
+			}
 
-				var winner = lottoCommands.rand();
-				if(winner != undefined)
-				{
-					msg.reply('and the winner is... \n' + winner + '!');
-				}
-				else
-				{
-					msg.reply("looks like there's no items in the lotto list! Maybe try using LottoAdd first.");
-				}
+		}
+		else
+		{
+			userHasPermissions = true;
+		}
+		
+		if(userHasPermissions)
+		{
+			switch (command)
+			{
+				case 'help':
+					var helpString = GetHelpString();
+					console.log(helpString);
+					channel.send(helpString);
+					break;
+					
+				case 'ping':
+					msg.reply('pong MOTHER FUCKER');
+					break;
+					
+				case 'api':
+					msg.reply('https://discord.js.org/');
+					break;
+					
+				case 'lottoadd':
 				
-				break;
+					var addTos = [];
+					if(params[2] != undefined)//Just check there's atleast one
+					{
+						//Adds multiple.
+						for(var i = 2; i < params.length; i++)//Go through params, make this a bit more dynamic.
+						{
+							addTos[i-2] = params[i];
+						}
+					}
+					
+					lottoCommands.add(addTos);
+					
+					break;
 				
-			case 'lottowinner':
+				case 'lottoclear':
+	
+					lottoCommands.clear();
+					msg.react('👍');
+					
+					break;
+					
+				case 'lottoall':
+	
+					var lotList = lottoCommands.all();
+					if(lotList != undefined)
+					{
+						channel.send("So far in the Lotto list: " + lottoCommands.all());	
+					}
+					else
+					{
+						channel.send('Lotto list is empty. Use LottoAdd command to add to list.');
+					}
+							
+					break;
 				
-				msg.reply(lottoCommands.winner());
-				
-				break;
-				
-			case 'checkserver':
-				var hosts = ['google.com', '46.251.234.220'];
-				hosts.forEach(function(host){
-					ping.sys.probe(host, function(isAlive){
-						var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-						channel.send(msg);
-						console.log(msg);
+				case 'lottorand':
+	
+					var winner = lottoCommands.rand();
+					if(winner != undefined)
+					{
+						msg.reply('and the winner is... \n' + winner + '!');
+					}
+					else
+					{
+						msg.reply("looks like there's no items in the lotto list! Maybe try using LottoAdd first.");
+					}
+					
+					break;
+					
+				case 'lottowinner':
+					
+					msg.reply(lottoCommands.winner());
+					
+					break;
+					
+				case 'checkserver':
+					var hosts = ['google.com', '46.251.234.220'];
+					hosts.forEach(function(host){
+						ping.sys.probe(host, function(isAlive){
+							var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+							channel.send(msg);
+							console.log(msg);
+						});
 					});
-				});
-				break;
-			
-			case 'tilleytest':
-				var test = TilleyTest.testing;		
-				//run function from variable in TilleyTest;
-				channel.send(TilleyTest.getKieran());
-				//print string variable i tilley test
-				channel.send(test);
-				break;
+					break;
 				
-			default:
-				console.log('no command found');
-				msg.reply("There is no command: < " + command + " > check your shit");
+				case 'tilleytest':
+					var test = TilleyTest.testing;		
+					//run function from variable in TilleyTest;
+					channel.send(TilleyTest.getKieran());
+					//print string variable i tilley test
+					channel.send(test);
+					break;
+					
+				case 'kieranexperimental':
+				
+				var roles = [];
+				for(var i = 2; i < params.length; i++)//Go through params, make this a bit more dynamic.
+				{
+					var thisRole = msg.guild.roles.find("name", params[i]);
+					console.log('Adding role: ' + thisRole.name);
+					allowedRoles.push(thisRole);
+					roles[i-2] = thisRole;
+				} 
+				
+				//allowedRoles.push(roles);
+				
+				break;
+					
+				default:
+					console.log('no command found');
+					msg.reply("There is no command: < " + command + " > check your shit");
+			}
 		}		
 	}
 });
