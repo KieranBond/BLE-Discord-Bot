50e27b18e812347d96c7d7bcc1c3b3f689278c35
Kieran Bond <Kieranbond97@gmail.com>
2018-10-10 16:16:18 +0100

    Test 2

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const thief = require("./scripts/thief");
 const prefix = "!ble ";
 const theifPre = "!thief";
 
-const repoLink = "https://github.com/KieranBond/BLE-Discord-Bot"; //Update this if needed!
+const repoLink = "https://github.com/KieranBond/BLE-Discord-Bot"; 
 
 var allowedRoles = [];
 
